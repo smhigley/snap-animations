4df78cd7105dce504e971ac8d6d57ab607841cf2
smhigley <sarahmhigley@gmail.com>
2014-03-26 00:39:33 -0500

    basic styles and svg append script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+// Snap SVG code
 
+window.onload = function () {
+  // wrap svg on page with Snap
+  var share = Snap('#share');
+
+  // load svg file
+  Snap.load("../img/animated-share.svg", function (s) {
+    // now save some vector groups into variables
+    var speech = s.select('#speech'),
+        phone = s.select('#phone'),
+        submit = s.select('#submit'),
+        text = s.select('#text');
+
+    // add speech bubble to share svg
+    share.append(speech);
+  });
+}
