0edca4dc3c2f749ff2ba07b306754b52ebc657dc
smhigley <sarahmhigley@gmail.com>
2014-03-26 22:48:14 -0500

    Button animation and animation on hover

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ window.onload = function () {
   Snap.load("img/animated-share.svg", function (s) {
     // now save some vector groups into variables
     var speech = s.select('#speech'),
+        pointer = speech.select('#pointer'),
         phone = s.select('#phone'),
         submit = s.select('#submit'),
         text = s.select('#text'),
@@ -16,26 +17,22 @@ window.onload = function () {
         frame = phone.select("#frame"),
         screenbg = phone.select("#background");
 
-    // hide phone frame and submit button
-    // and set them up to be animated
-    // but first we need the phone's bounding box
+    // we also need the phone's bounding box for some calculations
     var phonebox = screenbg.getBBox();
-    console.log(phonebox);
 
-    //now the transforms
-    submit.transform("t0,-25");
-    frame.transform("s0.85, 0.56, " + phonebox.cx + ", " + phonebox.y + " t0,19");
-    screenbg.transform("s1, 0.75, " + phonebox.cx + ", " + phonebox.y );
-    button.transform("s0,0");
-    speaker.transform("s0,0");
-
-    //now append the svg
-    share.append(s);
+    //now transform the paths to their pre-animation state
+    function reset() {
+      pointer.transform("t0,0");
+      submit.transform("t0,-25");
+      frame.transform("s0.85, 0.56, " + phonebox.cx + ", " + phonebox.y + " t0,19");
+      screenbg.transform("s1, 0.75, " + phonebox.cx + ", " + phonebox.y );
+      button.transform("s0,0");
+      speaker.transform("s0,0");
+    }
 
     // let's break down the animations into steps that accept delay and animation length as args
     // 1. animate the speech bubble pointer
     function step1(delay, length) {
-      var pointer = speech.select('#pointer');
       pointer.animate({
         transform: "t50,0"
       }, length, mina.backin);
@@ -72,7 +69,7 @@ window.onload = function () {
       }, delay);
     }
 
-    // 4. animate the speaker and button appearing
+    // 5. animate the speaker and button appearing
     function step5(delay, length) {
       setTimeout(function(){
         speaker.animate({
@@ -85,13 +82,48 @@ window.onload = function () {
       }, delay);
     }
 
+    // 6. button press
+    function step6(delay, length) {
+      var submit_bg = submit.select('#button_top'),
+          submit_text = submit.select('#submit-text');
+      var button_top = submit.group(submit_bg, submit_text);
+      setTimeout(function(){
+        //button down
+        button_top.animate({
+          transform: "t0,3"
+        }, length/2, mina.easein, function(){
+          //button up
+          button_top.animate({
+            transform: "t0,0"
+          }, length/2, mina.easeout);
+        });
+      }, delay);
+    }
 
-    //now call the steps
-    step1(0, 500);
-    step2(500, 800);
-    step3(800, 300);
-    step4(1100, 300);
-    step5(1400, 400);
+    // group all the steps in a function
+    function animate_share() {
+      step1(0, 500);
+      step2(500, 800);
+      step3(800, 300);
+      step4(1100, 300);
+      step5(1400, 400);
+      step6(1800, 200);
+    }
+
+    // on load, first reset the paths
+    reset()
+    //now append the svg
+    share.append(s);
+    // animate once
+    animate_share();
+
+    // then animate when hovered
+    phone.mouseover(function() {
+      animating = frame.inAnim();
+      if(animating) { console.log(s); }
+      reset();
+      animate_share();
+    });
 
   });
 }
